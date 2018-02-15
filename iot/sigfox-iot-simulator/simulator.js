d99124fc8c7bdf379d83d7846c1ebb9f8c843265
JeromeChaaban <jerome.chaaban@gmail.com>
2018-02-15 15:22:57 +0100

    Update simulator.js

--- iot/sigfox-iot-simulator/simulator.js
+++ iot/sigfox-iot-simulator/simulator.js
@@ -53,7 +53,7 @@ data: {3}`;
     let result = pattern.format(device["name"],Math.floor(Date.now() / 1000),randomIntBetween1And(1000000),device.generator());
     console.log(result);
     try{
-      let response = await got.post(url,{body:JSON.stringify({"data":"new"})});
+      let response = await got.post(url,{body:result});
       console.log("Successfully joined",url);
     }catch(error){
       console.log("Unable to join",url);
